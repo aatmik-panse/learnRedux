e8466ad56184062d5b95f57121910aae3931b9d3
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-06-11 08:33:48 +0530

    action and reducer setup

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,9 +1,9 @@
 const Shop = () => {
   return (
     <div>
-      <h1>Buy Shoes</h1>
+      <h1>Deposit / Withdraw Money </h1>
       <button className="btn btn-primary m-2">-</button>
-      Add To Cart
+      Update Balance
       <button className="btn btn-primary m-2">+</button>
     </div>
   );
