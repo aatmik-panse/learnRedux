69db3b3a54a7722d744b5ef5eeb3cdccffaec290
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-06-15 17:30:35 +0530

    final commit

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,25 +1,26 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { action } from "./state";
 import { bindActionCreators } from "redux";
 const Shop = () => {
   const dispatch = useDispatch();
-  const actions = bindActionCreators(action, dispatch);
+  const { withdrawMoney, depositMoney } = bindActionCreators(action, dispatch);
+  const balance = useSelector((state) => state.amount);
   return (
     <div>
       <h1>Deposit / Withdraw Money </h1>
       <button
         className="btn btn-primary m-2"
         onClick={() => {
-          actions.withdrawMoney(100);
+          withdrawMoney(100);
         }}
       >
         -
       </button>
-      Update Balance
+      Updated Balance : {balance}
       <button
         className="btn btn-primary m-2"
         onClick={() => {
-          actions.depositMoney(100);
+          depositMoney(100);
         }}
       >
         +
