6bac7e4d369674e4b874fa7b746591fe447174f9
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-06-15 18:21:11 +0530

    using redux

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,10 +1,28 @@
+import { useDispatch } from "react-redux";
+import { action } from "./state";
 const Shop = () => {
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Deposit / Withdraw Money </h1>
-      <button className="btn btn-primary m-2">-</button>
+      <button
+        className="btn btn-primary m-2"
+        onClick={() => {
+          dispatch(action.withdrawMoney(100));
+        }}
+      >
+        -
+      </button>
       Update Balance
-      <button className="btn btn-primary m-2">+</button>
+      <button
+        className="btn btn-primary m-2"
+        onClick={() => {
+          dispatch(action.depositMoney(200));
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
